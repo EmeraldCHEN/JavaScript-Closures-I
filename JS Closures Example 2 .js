9cede2ffdd6f4d52df97c57e1a6fea9fe38da37b
Emerald Chen <ahyuancy@gmail.com>
2019-01-17 14:34:40 +1300

    Update and rename JS Closures Example.js to JS Closures Example 2 .js

--- JS Closures Example 2 .js
+++ JS Closures Example 2 .js
@@ -2,11 +2,16 @@ function Bird(){
     // private property
     let hatchedEgg = 10; 
 
+    /* Creating a variable within the constructor function to change the scope of that variable to be within the constructor function versus available globally. 
+    This way, the property can only be accessed and changed by methods also within the constructor function.*/
+    
     // publicly available method that a bird object can use
-    this.getHatchedEggCount = function(){ // a privileged method, because it has access to the private variable hatchedEgg which is declared in the same context as getHachedEggCount
+    this.getHatchedEggCount = function(){ /* a privileged method, because it has access to the private variable hatchedEgg 
+                                             which is declared in the same context as getHachedEggCount   */
         return hatchedEgg;
     };
 }
+
 let ducky = new Bird();
 
 // ducky.hatchedEgg(); -> TypeError: ducky.hatchedEgg is not a function
